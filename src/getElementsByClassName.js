7fed22e2c187b665dc937c9bfe0b6e3e277c66db
jotrumo <jtmontoya26@gmail.com>
2020-05-11 17:13:04 -0700

    Updated spec and src file - working

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,29 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//use document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  //recursive function
+  function getClassName(element) {
+    //if exists, push to result
+    if (element.classList && element.className.indexOf(className) > -1) {
+      result.push(element);
+    }
+    //check if childNodes
+    if (element.childNodes) {
+      //if true, iterate values recursively
+      var values = Object.values(element.children);
+      for (var i = 0; i < values.length; i++) {
+        getClassName(values[i]);
+      }
+    }
+  }
+  //invokes recursive function
+  getClassName(document.body);
+
+  return result;
 };
